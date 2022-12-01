aed41436e1df00092d299253303b1fbc393a8dd5
Artemis Livingstone <user@test.com>
2022-12-01 16:00:32 +0000

    Add long one line version

--- src/tasks/task1.rs
+++ src/tasks/task1.rs
@@ -1,4 +1,10 @@
-use std::{cmp, usize};
+use core::num;
+use std::{
+    cmp::{self, Reverse},
+    usize,
+};
+
+use itertools::Itertools;
 
 use crate::task_handler::get_task;
 
@@ -6,21 +12,75 @@ pub fn tasks() {
     println!("{}", task1());
     println!("{}", task2());
 }
-
-fn total_calories() -> std::vec::Vec<usize> {
-    let task = get_task(1);
-    let lines = task.lines().collect::<Vec<_>>();
-    lines
-        .split(|x| x == &"")
-        .map(|x| x.iter().map(|x| x.parse::<usize>().unwrap()).sum::<usize>())
-        .collect::<Vec<_>>()
-}
 fn task1() -> usize {
     *total_calories().iter().max().unwrap()
 }
 
 fn task2() -> usize {
-    let mut total = total_calories();
-    total.sort_unstable_by_key(|x| cmp::Reverse(*x));
-    total[0..3].iter().sum()
+    total_calories()
+        .iter()
+        .fold([0_usize, 0_usize, 0_usize], |mut curr_top_3, x| {
+            if *x > curr_top_3[2] {
+                // Replace lowest
+                curr_top_3[2] = *x;
+                // Re-sort
+                curr_top_3.sort_unstable_by_key(|x| Reverse(*x));
+            }
+            curr_top_3
+        })
+        .iter()
+        .sum()
+}
+
+fn total_calories() -> Vec<usize> {
+    let task = get_task(1);
+    task.lines()
+        .group_by(|x| x != &"")
+        .into_iter()
+        .map(|x| x.1.map(|x| x.parse::<usize>().unwrap_or(0)).sum::<usize>())
+        .collect()
+}
+
+fn task1_long() -> usize {
+    let task = get_task(1);
+    task.lines()
+        .group_by(|x| x != &"")
+        .into_iter()
+        .map(|x| x.1.map(|x| x.parse::<usize>().unwrap_or(0)).sum::<usize>())
+        .max()
+        .unwrap()
+}
+
+fn task2_long() -> usize {
+    let task = get_task(1);
+    task.lines()
+        .group_by(|x| x != &"")
+        .into_iter()
+        .map(|x| x.1.map(|x| x.parse::<usize>().unwrap_or(0)).sum::<usize>())
+        .fold([0_usize, 0_usize, 0_usize], |mut curr_top_3, x| {
+            if x > curr_top_3[2] {
+                // Replace lowest
+                curr_top_3[2] = x;
+                // Re-sort
+                curr_top_3.sort_unstable_by_key(|x| Reverse(*x));
+            }
+            curr_top_3
+        })
+        .iter()
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::{task1, task2};
+
+    #[test]
+    fn test_task1() {
+        assert_eq!(72511, task1());
+    }
+
+    #[test]
+    fn test_task2() {
+        assert_eq!(212_117, task2());
+    }
 }
